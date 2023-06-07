d7981e253674dd0920784061705fec57ec9b0a4d
Atara Rose <ataraeg613@gmail.com>
2023-06-07 10:28:54 +0300

    user login works, return token, when running make sure to enter your db user and db password in the secret.js file, or in mongo db

--- config/secret.js
+++ config/secret.js
@@ -1,7 +1,10 @@
-// require("dotenv").config()
+require("dotenv").config()
 
-// exports.config = {
+exports.config = {
+  userDb:"atara@gmail.com",
+  passDb:"YissiAtara121299!",
+  tokenSecret:"mySecret"
 //   userDb:process.env.USER_DB,
 //   passDb:process.env.PASS_DB,
 //   tokenSecret:process.env.TOKEN_SECRET
-// }
+}
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const express= require("express");
 //const {auth, authAdmin} = require("../middlewares/auth");
 const {UserModel} = require("../models/userModel");
 const bcrypt = require("bcrypt");
-const {validateUser} = require("../validation/userValidation")
+const {validateUser,validateLogin} = require("../validation/userValidation")
+const{createToken}=require("../helperFunctions/userHelper")
 const router = express.Router();
 
 
@@ -42,6 +43,31 @@ router.post("/",async (req, res) => {
   }
 });
 
+//user login to db
+router.post("/login",async (req, res) => {
+    let validBody = validateLogin(req.body);
+    if (validBody.error) {
+      return res.status(400).json(validBody.error.details);
+    }
+    try {
+      //check if the email exists in the db
+      let user = await UserModel.findOne({ email: req.body.email })
+      if (!user) {
+        return res.status(401).json({ msg: "Password or email is incorrect ,code:1" })
+      }
+      // check if the password entered matches the encrypted password in the db
+      let isPassword = await bcrypt.compare(req.body.password, user.password);
+      if (!isPassword) {
+        return res.status(401).json({ msg: "Password or email is incorrect ,code:2" });
+      }
+      //Generate a token with the user's ID
+      let token = createToken(user._id, user.role);
+      res.json({ token });
+    }
+    catch (err) {
+      console.log(err)
+      res.status(500).json({ msg: "error in user login", err })
+    }
+  });
+
 module.exports = router;
-
-
