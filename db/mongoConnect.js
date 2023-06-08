dec24a0ec2b8f3232b290a4a9175e421558009cc
Elisheva (Aronstam) Sherman <57481785+ElishAronstam@users.noreply.github.com>
2023-06-08 14:55:45 +0300

    cvvb

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -4,8 +4,9 @@ const {config}=require('../config/secret')
 main().catch(err => console.log(err));
 
 async function main() {
-
-  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster1.mqgr2av.mongodb.net/Toys_Api`);
+  await mongoose.connect(`mongodb+srv://elisheva777:eli777me@cluster1.mqgr2av.mongodb.net/Toys_Api`);
+  // mongodb+srv://elisheva777:eli777me@cluster1.mqgr2av.mongodb.net/
+  // await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster1.mqgr2av.mongodb.net/Toys_Api`);
   console.log("mongo connect");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
