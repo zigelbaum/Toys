57a61a5e2172d301a86e6de0c630583091be5fe9
Elisheva (Aronstam) Sherman <57481785+ElishAronstam@users.noreply.github.com>
2023-06-08 15:01:33 +0300

    rrr

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -4,7 +4,7 @@ const {config}=require('../config/secret')
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb+srv://elisheva777:eli777me@cluster1.mqgr2av.mongodb.net/Toys_Api`);
+  await mongoose.connect(`mongodb+srv://{config.userDb}:{config.passDb}@cluster1.mqgr2av.mongodb.net/Toys_Api`);
   // mongodb+srv://elisheva777:eli777me@cluster1.mqgr2av.mongodb.net/
   // await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster1.mqgr2av.mongodb.net/Toys_Api`);
   console.log("mongo connect");
