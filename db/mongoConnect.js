62199a4a299a9331224227a6b42b60fee0d085e6
Elisheva (Aronstam) Sherman <57481785+ElishAronstam@users.noreply.github.com>
2023-06-08 14:06:21 +0300

    dd

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -5,7 +5,7 @@ main().catch(err => console.log(err));
 
 async function main() {
     
-  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster1.mqgr2av.mongodb.net/black`);
+  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster1.mqgr2av.mongodb.net/Toys_api`);
   console.log("mongo connect");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
